8658d9853d8f13ae02b7d59cf17f592392853e67
Benjamin Poon <benjamintpoon@gmail.com>
2014-07-08 10:07:51 -0500

    map dance animations to keypresses 1-4

--- public/js/moves.js
+++ public/js/moves.js
@@ -1,5 +1,8 @@
 $(function() {
   
+  var dancer = document.getElementById('dancer');
+  var $dancerContainer = $(dancer).parent();
+
   window.moves = [];
   $(document).on('mousemove', function(e) {
       moves.push({
@@ -18,7 +21,6 @@ $(function() {
       var angleX = (mouseY / height) * 90;
       var angleY = (mouseX / width) * 90;
 
-      var dancer = document.getElementById('dancer');
       dancer.style.webkitTransform = "rotateX(" + angleX + "deg) rotateY(" + angleY + "deg)";
   });
 
@@ -29,46 +31,56 @@ $(function() {
 
   $(window).on('resize', resize);
   $(document).ready(resize);
+
+  $(document).on('keypress', function(e) {
+    switch (e.which) {
+      case 49:
+        triggerAnimation($dancerContainer, 'shake');
+        break;
+      case 50:
+        triggerAnimation($dancerContainer, 'bounce');
+        break;
+      case 51:
+        triggerAnimation($dancerContainer, 'swing');
+        break;
+      case 52:
+        triggerAnimation($dancerContainer, 'wobble');
+        break;
+    }
+  });
   
+  function triggerAnimation(el, klassName) {
+    el.stop(true, false).addClass(klassName).one('webkitAnimationEnd', function() { el.removeClass(klassName) });
+    // el.stop();
+    // el.addClass(klassName);
+    // el.one('webkitAnimationEnd', function() { 
+    //   el.removeClass(klassName) ;
+    // });
+  }
   
-  $('#shake').on('click', function(){
-      $('.dancer').addClass('animated shake');
-        $('.dancer').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-       $('.dancer').removeClass('animated shake');
-          });
-        });
-    $('#wobble').on('click', function(){
-      $('.dancer').addClass('animated wobble');
-        $('.dancer').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-          $('.dancer').removeClass('animated wobble');
-           });
-        });
-    $('#swing').on('click', function(){
-      $('.dancer').addClass('animated swing');
-        $('.dancer').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-          $('.dancer').removeClass('animated swing');
-            });
-        });
-    $('#bounce').on('click', function(){
-      $('.dancer').addClass('animated bounce');
-        $('.dancer').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-          $('.dancer').removeClass('animated bounce');
-            });
-        });
-
-
-  $('#bounceInRight').on('click', function(){
-      $('.dancer').addClass('animated bounceInRight');
-        $('.dancer').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-          $('.dancer').removeClass('animated bounceInRight');
-            });
-        });
-
-  $('#bounceInLeft').on('click', function(){
-      $('.dancer').addClass('animated bounceInLeft');
-        $('.dancer').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-          $('.dancer').removeClass('animated bounceInLeft');
-            });
-        });
+  // $('#shake').on('click', function(){
+  //     $('.dancer').addClass('animated shake');
+  //       $('.dancer').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+  //      $('.dancer').removeClass('animated shake');
+  //         });
+  //       });
+  //   $('#wobble').on('click', function(){
+  //     $('.dancer').addClass('animated wobble');
+  //       $('.dancer').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+  //         $('.dancer').removeClass('animated wobble');
+  //          });
+  //       });
+  //   $('#swing').on('click', function(){
+  //     $('.dancer').addClass('animated swing');
+  //       $('.dancer').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+  //         $('.dancer').removeClass('animated swing');
+  //           });
+  //       });
+  //   $('#bounce').on('click', function(){
+  //     $('.dancer').addClass('animated bounce');
+  //       $('.dancer').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+  //         $('.dancer').removeClass('animated bounce');
+  //           });
+  //       });
   
 });
